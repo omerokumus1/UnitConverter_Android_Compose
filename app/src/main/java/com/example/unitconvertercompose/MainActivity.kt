c624f46f4bc27dd94421ea4a99a78ec6dd5829e3
Ömer Okumuş <omerokumus3@gmail.com>
2024-06-27 12:50:47 +0300

    Implementing state and converting logic

--- app/src/main/java/com/example/unitconvertercompose/MainActivity.kt
+++ app/src/main/java/com/example/unitconvertercompose/MainActivity.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
@@ -25,6 +24,11 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -34,6 +38,26 @@ import androidx.compose.ui.unit.dp
 import com.example.unitconvertercompose.ui.theme.UnitConverterComposeTheme
 
 class MainActivity : ComponentActivity() {
+    companion object
+    {
+        val converterMatrix = mapOf(
+            "Centimeters" to mapOf(
+                "Centimeters" to 1.0,
+                "Meters" to 0.01,
+                "Kilometers" to 0.00001
+            ),
+            "Meters" to mapOf(
+                "Centimeters" to 100.0,
+                "Meters" to 1.0,
+                "Kilometers" to 0.001
+            ),
+            "Kilometers" to mapOf(
+                "Centimeters" to 100000.0,
+                "Meters" to 1000.0,
+                "Kilometers" to 1.0
+            )
+        )
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -53,31 +77,58 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun UnitConverter() {
+    var valueToConvert by remember { mutableStateOf("") }
+    var result by remember { mutableStateOf("") }
+    val from = remember { mutableStateOf("") }
+    val to = remember { mutableStateOf("") }
+    val context = LocalContext.current
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Text(text = "Unit Converter")
+        Text(text = "Unit Converter", style = MaterialTheme.typography.headlineMedium)
         Spacer(modifier = Modifier.height(16.dp))
-        OutlinedTextField(value = "", onValueChange = {})
+        OutlinedTextField(
+            value = valueToConvert,
+            onValueChange = { valueToConvert = it },
+            placeholder = {
+                Text(text = "Enter value to convert")
+            })
         Spacer(modifier = Modifier.height(16.dp))
         Row {
-            MetricsDropdownButton("From")
+            MetricsDropdownButton("From", from)
             Spacer(modifier = Modifier.width(16.dp))
-            MetricsDropdownButton("To")
+            MetricsDropdownButton("To", to)
 
 
         }
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = {
+            valueToConvert.toDoubleOrNull()?.let {
+                val conversionFactor = MainActivity.converterMatrix[from.value]?.get(to.value)
+                if (conversionFactor != null) {
+                    result = (it * conversionFactor).toString()
+                } else {
+                    Toast.makeText(context, "Conversion not supported", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }) {
+            Text(text = "Convert")
+        }
+        Text(text = "Converted Value: $result ${to.value}")
     }
 }
 
 @Composable
-fun MetricsDropdownButton(text: String) {
-    Box {
-        val context = LocalContext.current
+fun MetricsDropdownButton(text: String, selectedValue: MutableState<String>) {
+    val isDropdownExpanded = remember { mutableStateOf(false) }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Button(onClick = {
-            Toast.makeText(context, "Button clicked", Toast.LENGTH_SHORT).show()
+            isDropdownExpanded.value = !isDropdownExpanded.value
         }) {
             Text(text)
             Icon(
@@ -85,17 +136,27 @@ fun MetricsDropdownButton(text: String) {
                 contentDescription = "Drop down icon"
             )
         }
-        MetricsDropdownMenu()
-
+        Text(text = selectedValue.value)
+        MetricsDropdownMenu(isDropdownExpanded, selectedValue)
     }
 }
 
 @Composable
-fun MetricsDropdownMenu(textList: List<String> = listOf("Centimeters",
-    "Meters", "Kilometers", "Feet", "Milimeters")) {
-    DropdownMenu(expanded = true, onDismissRequest = { /*TODO*/ }) {
+fun MetricsDropdownMenu(
+    isDropdownExpanded: MutableState<Boolean>,
+    selectedValue: MutableState<String>,
+    textList: List<String> = listOf(
+        "Centimeters", "Meters", "Kilometers",
+    )
+) {
+    DropdownMenu(expanded = isDropdownExpanded.value, onDismissRequest = {
+        isDropdownExpanded.value = false
+    }) {
         textList.map {
-            DropdownMenuItem(text = { Text(it) }, onClick = { /*TODO*/ })
+            DropdownMenuItem(text = { Text(it) }, onClick = {
+                selectedValue.value = it
+                isDropdownExpanded.value = false
+            })
         }
     }
 }
